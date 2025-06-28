0a10078cbc4ffb19bc8b623945ec3227aa3020ee
MuriloJrMarques <muriloj2702@gmail.com>
2025-06-28 20:43:52 -0300

    create routes(get, create, post, getEdit, put, delete) in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuncionarioController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FuncionarioController::class, 'index'])->name('funcionarios.index');
+Route::get('/funcionarios/create', [FuncionarioController::class, 'create'])->name('funcionarios.create');
+Route::post('/funcionarios', [FuncionarioController::class, 'store'])->name('funcionarios.store');
+Route::get('/funcionarios/{id}/edit', [FuncionarioController::class, 'edit'])->name('funcionarios.edit');
+Route::put('/funcionarios/{id}', [FuncionarioController::class, 'update'])->name('funcionarios.update');
+Route::delete('/funcionarios/{id}', [FuncionarioController::class, 'destroy'])->name('funcionarios.destroy');
