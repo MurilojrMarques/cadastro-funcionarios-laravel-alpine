9527183fe1c5c7e7a5ebf1b6b57dba63512a7963
MuriloJrMarques <muriloj2702@gmail.com>
2025-06-28 21:37:11 -0300

    fixes FuncionarioController imports Models and Request

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Funcionario;
+use App\Http\Requests\FuncionarioRequest;
 
 class FuncionarioController extends Controller
 {
